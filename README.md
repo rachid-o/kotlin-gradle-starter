8fa7b8f7554111d05c85bb621e65616cd46e4689
Rachid Ben Moussa <rachid.benmoussa@luminis.eu>
2019-08-27 21:40:48 +0200

    Added dependencies for Kotlin

---
+++ README.md
@@ -0,0 +1,7 @@
+
+# Getting started with Kotlin.
+
+Compile and run the example using Gradle:
+` ./gradlew run`
+ 
+
---
+++ src/main/kotlin/HelloWorld.kt
@@ -0,0 +1,6 @@
+/**
+ * This is executed by default when running Gradle
+ */
+fun main(args: Array<String>) {
+    println("Hello world!")
+}
